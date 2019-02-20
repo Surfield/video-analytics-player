72669bad45667c8b2a93de5cf53219d4063f00a5
Surfield <SurfieldA@gmail.com>
2019-02-19 23:03:33 -0500

    add: code for players

---
+++ src/components/Mp4Player.vue
@@ -0,0 +1,17 @@
+<template>
+    <div :class="videoType">
+            <video :poster="poster" :src="videoId" controls>
+                <source :src="videoId" type="video/mp4" size="1080">
+            </video>
+    </div>
+</template>
+<script>
+export default {
+  name: 'Mp4Player',
+  props: {
+    videoType: String,
+    poster: String,
+    videoId: String
+  }
+}
+</script>
---
+++ src/components/YoutubeVimeoPlayer.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <div :class="videoType">
+        <vue-plyr>
+            <div :data-plyr-provider="videoType" :data-plyr-embed-id="videoId"></div>
+        </vue-plyr>
+    </div>
+</template>
+<script>
+export default {
+  name: 'YoutubeVimeoPlayer',
+  props: {
+    videoType: String,
+    videoId: String
+  }
+}
+</script>
