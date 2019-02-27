58a2c4e0d3d4e1faac6c1f0df34a5d8979b4334e
Surfield <SurfieldA@gmail.com>
2019-02-27 00:42:51 -0500

    send to backend

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VuePlyr from 'vue-plyr'
 import 'vue-plyr/dist/vue-plyr.css'
+require('dotenv').config();
 
 Vue.use(VuePlyr)
 Vue.config.productionTip = false
--- src/utils/captureEvent.js
+++ src/utils/captureEvent.js
@@ -1,4 +1,5 @@
 var pjson = require('../../package.json');
+import axios from 'axios'
 
 export function captureEvent(eventName, eventTime){
     console.log(eventName +' event fired at ' + parseInt(eventTime))
@@ -27,9 +28,22 @@ export function sendVideoEventData(eventName, eventTime, meta){
     let payload = eventDataFormatter(eventName, eventTime, meta)
     
     createVideoEvent(payload)
+    sendToBackend(payload)
 }
 
 function createVideoEvent(payload){
     var event = new CustomEvent('videoAnalyticsPlayerEvent', { detail: payload });
     dispatchEvent(event);
+}
+
+function sendToBackend(payload){
+    if (process.env.VUE_APP_VIDEO_ANALYTICS_PLAYER_BACKEND){
+        axios.post('https://httpbin.org/post', payload)
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+    }
 }
